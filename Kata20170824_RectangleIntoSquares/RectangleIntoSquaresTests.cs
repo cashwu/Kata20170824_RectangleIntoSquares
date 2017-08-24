cfd33336d4e575819564b7ec34db9aa44634e17f
cash <cash@cashwu.com>
2017-08-24 17:12:24 +0800

    input 2, 2 test case

--- Kata20170824_RectangleIntoSquares/RectangleIntoSquaresTests.cs
+++ Kata20170824_RectangleIntoSquares/RectangleIntoSquaresTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170824_RectangleIntoSquares
@@ -7,8 +9,24 @@ namespace Kata20170824_RectangleIntoSquares
     public class RectangleIntoSquaresTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_2_2_should_return_null()
         {
+            SqInRectShouldBe(null, 2, 2);
+        }
+
+        private static void SqInRectShouldBe(List<int> expected, int lng, int wdth)
+        {
+            var s = new SqInRect();
+            var actual = s.sqInRect(lng, wdth);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class SqInRect
+    {
+        public List<int> sqInRect(int lng, int wdth)
+        {
+            return null;
         }
     }
 }
